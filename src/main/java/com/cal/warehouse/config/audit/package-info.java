2ddcf37393b0b513fe599cbfcbdbb736e96e67e7
Trandtb <trandtb@greenglobal.vn>
2018-09-04 17:19:32 +0700

    init

---
+++ src/main/java/com/cal/warehouse/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.cal.warehouse.config.audit;
---
+++ src/main/java/com/cal/warehouse/web/websocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * WebSocket services, using Spring Websocket.
+ */
+package com.cal.warehouse.web.websocket;
